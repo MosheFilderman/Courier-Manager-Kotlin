43255f14266b23f9c5d892bc0791166c6055ec3f
SERGI\Sergey <sergeyda95@gmail.com>
2023-05-10 13:54:38 +0300

    Customer view: #bottom menu #fragment container for: new order & customer order list

---
+++ app/OrderListFragment.kt
@@ -0,0 +1,9 @@
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+
+class OrderListFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?{
+        return inflater.inflate(R.layout.new_order_fragment, container, false)
+    }
+}
