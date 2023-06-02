a0b2834532f1e592e4bbc2198298e5b7aa2087e2
MosheFilderman <mfilderman@gmail.com>
2023-06-02 12:19:02 +0300

    improve inteface

--- app/src/main/java/com/example/couriermanagerkotlin/Courier.kt
+++ app/src/main/java/com/example/couriermanagerkotlin/Courier.kt
@@ -1,7 +1,12 @@
 package com.example.couriermanagerkotlin
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -9,9 +14,16 @@ class Courier : AppCompatActivity() {
 
     lateinit var navigationView: BottomNavigationView
     lateinit var selected:Fragment
+    lateinit var firstName:TextView
+    lateinit var lastName:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_courier)
+        var data: SharedPreferences = getSharedPreferences("savefile", Context.MODE_PRIVATE)
+        firstName = findViewById(R.id.firstName)
+        lastName = findViewById(R.id.lastName)
+        firstName.text = data.getString("firstName", "no data")
+        lastName.text = data.getString("lastName", "no data")
         supportFragmentManager.beginTransaction().replace(R.id.listmapview,CourierMapFragment()).commit()
 
         navigationView = findViewById(R.id.nav) as BottomNavigationView
@@ -32,4 +44,6 @@ class Courier : AppCompatActivity() {
             true
         }
     }
+
+
 }
--- app/src/main/java/com/example/couriermanagerkotlin/Login.kt
+++ app/src/main/java/com/example/couriermanagerkotlin/Login.kt
@@ -90,36 +90,6 @@ class Login : AppCompatActivity() {
         requestQueue.add(stringRequest)
     }
 
-//    fun getUsers() {
-//        val url: String = "http://10.0.0.7/courier_project/get.php"
-//        val stringReq =
-//            StringRequest(Request.Method.GET, url, Response.Listener<String> { response ->
-//                var strResp = response.toString()
-//                val jsonArray = JSONArray(strResp)
-//                val jsonResponse = jsonArray.getJSONObject(0)
-//                val jsonArray_users = jsonResponse.getJSONArray("users")
-//
-//                var str_user: String = ""
-//                for (i in 0 until jsonArray_users.length()) {
-//                    var jsonInner: JSONObject = jsonArray_users.getJSONObject(i)
-//                    str_user = str_user + "\n" + jsonInner.get("firstName")
-//                    str_user = str_user + "\n" + jsonInner.get("lastName")
-//                    str_user = str_user + "\n" + jsonInner.get("email")
-//                    str_user = str_user + "\n" + jsonInner.get("phone")
-//                    str_user = str_user + "\n" + jsonInner.get("password")
-//                    str_user = str_user + "\n" + jsonInner.get("eStatus")
-//                    str_user = str_user + "\n" + jsonInner.get("eRole")
-//                }
-//
-//                Toast.makeText(this, str_user, Toast.LENGTH_SHORT).show()
-//            },
-//                Response.ErrorListener { error ->
-//                    Toast.makeText(this, error.toString(), Toast.LENGTH_SHORT).show()
-//                }
-//            )
-//        val queue = Volley.newRequestQueue(this)
-//        queue.add(stringReq)
-//    }
 
     fun registration(view: View) {
         startActivity(Intent(this, Registration::class.java))
@@ -189,5 +159,7 @@ class Login : AppCompatActivity() {
 
 
     }
+
+
 
 }
