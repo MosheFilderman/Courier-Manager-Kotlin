f9e7a3b7f2004b8d475103834422f22ed4bbb3cb
MosheFilderman <mfilderman@gmail.com>
2023-09-22 08:12:00 +0300

    scan qr code

---
+++ app/src/main/java/com/example/couriermanagerkotlin/activities/courier/scanQr.kt
@@ -0,0 +1,11 @@
+package com.example.couriermanagerkotlin.activities.courier
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.couriermanagerkotlin.R
+
+class scanQr : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
