0c4e01529f948a2d9c6ed475dca8d3ae4ab8327a
SERGI\Sergey <sergeyda95@gmail.com>
2024-02-18 13:52:23 +0200

    Mpbe the errorMessage TextView to position inside the layout

--- app/src/main/java/com/example/couriermanagerkotlin/utilities/DBUtilities.kt
+++ app/src/main/java/com/example/couriermanagerkotlin/utilities/DBUtilities.kt
@@ -30,7 +30,7 @@ import org.json.JSONObject
 class DBUtilities {
 
     companion object {
-        const val ipv4Address: String = "10.100.102.14"
+        const val ipv4Address: String = "10.0.0.7"
         var measures = Measures(-1, -1, -1, -1)
         var orders = ArrayList<Order>()
         var streets = ArrayList<String>()
